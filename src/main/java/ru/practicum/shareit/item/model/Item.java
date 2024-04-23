f46f98dde03243ec135f72618b516c0558547c28
Vladislav <bakushkinv@yandex.ru>
2024-04-23 22:01:36 +0300

    feat: реализовать модель данных Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Long ownerId;
+    private Boolean available;
 }
